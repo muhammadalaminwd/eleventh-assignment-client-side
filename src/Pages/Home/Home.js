ed286830b60bbf4f31b00c2b70314407649af35f
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-05 03:12:40 +0600

    just-created-not-found-route

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Header from '../Shared/Header/Header';
+import About from './About/About';
 import Banner from './Banner/Banner';
+import Items from './Items/Items';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Items></Items>
+            <About></About>
         </div>
     );
 };
