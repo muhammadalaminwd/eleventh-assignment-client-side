2cae26eb96232c854d81aabf04ca0318a80977cf
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-04 19:29:39 +0600

    navbar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,20 +1,33 @@
+import { Button } from 'react-bootstrap';
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import logo from "../../../images/images/logo.png";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-        <Navbar bg="success" variant="dark">
-        <Container>
-        <Navbar.Brand href="#home">FRESH WAREHOUSE</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Items</Nav.Link>
-          <Nav.Link href="#pricing">Blogs</Nav.Link>
-        </Nav>
+        <Navbar bg="success" expand="lg">
+        <Container fluid>
+        <Navbar.Brand className='ms-5' as={Link} to="/">
+        <img src={logo} alt="" height={90}/>
+        </Navbar.Brand>
+          <Navbar.Brand href="#" className='text-light fs-3'>FRESH WAREHOUSE</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+            >
+              <Nav.Link href="#home" className='text-light ms-5'>Home</Nav.Link>
+              <Nav.Link href="#items" className='text-light'>Items</Nav.Link>
+              <Nav.Link href="#blog" className='text-light'>Blog</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Button variant="outline-success" className='text-light'>Sign Out</Button>
+            </Form>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-        </div>
     );
 };
 
